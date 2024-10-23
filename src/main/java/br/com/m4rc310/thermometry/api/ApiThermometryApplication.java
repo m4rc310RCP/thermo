4f10021872c56a34827ea6d357c5243dced717c4
m4rc310 <mlsilva@coamo.com.br>
2024-10-23 20:18:46 -0300

    first commit

---
+++ src/main/java/br/com/m4rc310/thermometry/api/ApiThermometryApplication.java
@@ -0,0 +1,13 @@
+package br.com.m4rc310.thermometry.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiThermometryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiThermometryApplication.class, args);
+	}
+
+}
